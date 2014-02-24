0f0c801b2c1d1fa2a1f2ef599e04e0cd31f0e858
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-02-23 18:51:37 -0800

    Twitter Bootstrap integration (v0.2)

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
 from config import config
+
+
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    bootstrap.init_app(app)
 
     from .talks import talks as talks_blueprint
     app.register_blueprint(talks_blueprint)
