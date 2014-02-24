bb75207b02f96bb9716d961613cce34577a60c49
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-02-24 08:46:27 -0800

    User avatars (v0.12)

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request
 from flask.ext.login import UserMixin
 from . import db, login_manager
 
@@ -19,6 +21,12 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(
+                self.email.encode('utf-8')).hexdigest()
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -29,6 +37,16 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or \
+               hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
 
 @login_manager.user_loader
