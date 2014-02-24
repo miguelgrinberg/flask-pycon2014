26b33317b33a32c1b384170b45cfb449023d5f39
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-02-23 23:26:30 -0800

    Login form (v0.9)

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, current_app, request, redirect, url_for
 from . import auth
+from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    if not current_app.config['DEBUG'] and not current_app.config['TESTING'] \
+            and not request.is_secure:
+        return redirect(url_for('.login', _external=True, _scheme='https'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/login.html', form=form)
